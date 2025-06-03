862eecb87b715b5deff6d2d56ab8e4459ce62f92
Barmanpriyanka <priyankabarman1604@gmail.com>
2025-06-03 14:52:21 +0530

    Dom manupilation in javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+// Select the div with ID "demo"
+const byId = document.getElementById("demo");
+
+// Modify its content and style
+byId.innerText = "🎯 I was accessed using getElementById!";
+byId.style.backgroundColor = "#e3f2fd";
